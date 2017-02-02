c95e58ad0e44db767ef0f62f4484c4f6267eaa00
Demo Kioussis <Demo.kioussis@gmail.com>
2017-02-02 00:09:11 -0500

    pathfinding with unity's navmesh

---
+++ Assets/Scripts/Navigation/BasicAgentNavigation.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.AI;
+using UnityEngine;
+
+
+[RequireComponent(typeof(NavMeshAgent))]
+public class BasicAgentNavigation : MonoBehaviour {
+
+    Transform target;
+    private Vector3 currentDestination;
+    GameObject clickEffect;
+    NavMeshAgent agent;
+
+    void Start() {
+        agent = GetComponent<NavMeshAgent>();
+    }
+	void Update () {
+        if (target.position != currentDestination) {
+            setDestination();
+        }
+	}
+    public void setTarget(Transform t) {
+        target = t;
+        setDestination();
+    }
+    void setDestination() {
+        currentDestination = target.position;
+        agent.SetDestination(currentDestination);
+    }
+
+
+}
---
+++ Assets/Scripts/Navigation/MouseController.cs
@@ -0,0 +1,76 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MouseController : MonoBehaviour {
+
+    Vector3 positionClick,positionCurrent,positionRelease;
+    
+    bool firstClick = false;
+    bool mouseHeld = false;
+    public float doubleClickTime;
+    float timeAtFirstClick;
+    void Update() {
+        if (Input.GetKeyDown(KeyCode.Mouse0)){
+            mouseClick();
+        }
+        checkSingleClick();
+
+        if (Input.GetKey(KeyCode.Mouse0)) {
+            mouseHold();
+        }
+        if (Input.GetKeyUp(KeyCode.Mouse0))
+        {
+            mouseRelease();
+        }
+
+    }
+
+    void findPosition(ref Vector3 p)
+    {
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        RaycastHit hit;
+        if (Physics.Raycast(ray, out hit))
+        {
+            p = hit.point;
+        }
+    }
+    void mouseClick() {
+        findPosition(ref positionClick);
+
+        if (!firstClick)
+        {
+            firstClick = true;
+            timeAtFirstClick = Time.time;
+        }
+        else if (firstClick && Time.time - timeAtFirstClick < doubleClickTime)
+        {
+            firstClick = false;
+            timeAtFirstClick = 0;
+            doubleClick();
+        }
+    }
+    void checkSingleClick() {
+        if (firstClick && Time.time - timeAtFirstClick > doubleClickTime)
+        {
+            firstClick = false;
+            singleClick();
+        }
+    }
+    void singleClick() {
+
+    }
+    void doubleClick() {
+    }
+
+    void mouseHold() {
+        findPosition(ref positionCurrent);
+    }
+
+    void mouseRelease()
+    {
+        findPosition(ref positionRelease);
+
+    }
+
+}
