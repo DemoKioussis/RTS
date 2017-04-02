5094a3b281666f7e19b55970bb1b5503cb24548b
Demo Kioussis <Demo.kioussis@gmail.com>
2017-04-02 19:12:35 -0400

    Probably finished nav for basic positions

--- Assets/Resources/Scripts/ProgrammingModel/Interactable Models/Player-owned Models/Unit Models/Base/Group/UnitGroup.cs
+++ Assets/Resources/Scripts/ProgrammingModel/Interactable Models/Player-owned Models/Unit Models/Base/Group/UnitGroup.cs
@@ -5,34 +5,53 @@ using UnityEngine;
 public class UnitGroup :MonoBehaviour{
 
     public List<UnitController> units;
-    public float groupMovementSpeed;
+    public bool arrived= false;
+    public float arriveRadius;
+    public float stopDist;
     NavMeshAgent agent;
     NavMeshPath path;
     Interactable interaction;
     Vector3 targetPosition;
-
+    Vector3 center;
 
     void Awake() {
         units = new List<UnitController>();
         agent = GetComponent<NavMeshAgent>();
+
     }
 
+    void FixedUpdate() {
+        center = getCenter();
+        if (Vector3.Distance(targetPosition, center) < arriveRadius && !arrived)
+        {
+            stopAgents();
+            arrived = true;
+        }
+    }
     void OnDrawGizmos() {
 #if UNITY_EDITOR
         Gizmos.color = Color.red;
-        Gizmos.DrawSphere(getCenter() + Vector3.up*0.5f ,0.1f);
+        Gizmos.DrawSphere(center + Vector3.up*0.5f ,arriveRadius);
         for (int i = 0; i < path.corners.Length - 1; i++)
             Debug.DrawLine(path.corners[i], path.corners[i + 1], Color.red);
+        Gizmos.color = Color.blue;
+        Gizmos.DrawSphere(center + Vector3.up * 0.5f, stopDist);
+
+
 #endif
     }
     public void moveTo(MapPos p) {
+        arrived = false;
         interaction = p;
-        Vector3 center = getCenter();
+        center = getCenter();
         path = new NavMeshPath();
+        targetPosition = p.getPosition();
         NavMesh.CalculatePath(center, p.getPosition(), NavMesh.AllAreas, path);
 
         foreach (UnitController u in units) {
             u.setPath(path);
+            u.setStoppingDistance(0);
+
         }
     }
 
@@ -40,14 +59,31 @@ public class UnitGroup :MonoBehaviour{
         return units.Count == 0;
     }
 
-    public Vector3 getCenter() {
+    private Vector3 getCenter() {
         Vector3 center = new Vector3(0, 0, 0);
 
         foreach (UnitController u in units) {
-            center += u.transform.position / units.Count;
+            center += u.transform.position;
         }
+        center = center / units.Count;
         return center;
+    }
 
+    private void stopAgents() {
+        UnitController closest = units[0];
+        foreach (UnitController u in units) {
+            if (u.distanceTo(targetPosition) < closest.distanceTo(targetPosition)) {
+                closest = u;
+
+            }
+        }
+
+        stopDist = Mathf.Sqrt(units.Count) * closest.getRadius() / 1.8f;
+        foreach (UnitController u in units)
+        {
+            u.setDestination(u.transform.position);
+            u.setStoppingDistance(stopDist);
+        }
     }
 
 
