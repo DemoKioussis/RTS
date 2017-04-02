5e264c5d158b2371c3c6805aa943ee247802d442
Dexter Kwok <dkwok.90@gmail.com>
2017-04-02 14:11:27 -0400

    Added a new building for resources and new scripts for mouse functionality

---
+++ Assets/Resources/Scripts/Mouse Controls/Spawn/ObjectOnCursor.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectOnCursor : MonoBehaviour {
+
+	public GameObject stub;
+	public GameObject gameLevel;
+
+	void Update()
+	{
+		if (Input.GetButton("LeftClick"))
+		{
+			Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+			if (Physics.Raycast (ray)) {
+				Debug.Log("Click on plane");
+				Instantiate (stub, transform.position, transform.rotation);
+			}
+		}
+	}
+}
