a79e11aaf3229f695d24a404e31705d631d3949a
Dexter Kwok <dkwok.90@gmail.com>
2017-04-07 15:33:45 -0400

    Building interaction with mouse cursor

--- Assets/Resources/Scripts/ProgrammingModel/Interactable Models/Player-owned Models/Building Models/IndustrialCenter.cs
+++ Assets/Resources/Scripts/ProgrammingModel/Interactable Models/Player-owned Models/Building Models/IndustrialCenter.cs
@@ -3,15 +3,9 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class IndustrialCenter : ResourceBuilding {
-
-	// Use this for initialization
-	void Start () {
-		
-	}
 	
 	// Update is called once per frame
 	void Update () {
-		
 	}
 
 	protected override void Interaction(Interactable newInteraction)
