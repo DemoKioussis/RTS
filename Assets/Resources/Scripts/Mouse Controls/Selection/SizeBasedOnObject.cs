c41741f9bffc4680e0c6c77c4d777d9996edef8e
David Katchouni <David.Katchouni@adsdata.ca>
2017-04-07 14:00:36 -0400

    Mouse clicking a single unit Selection circle sizing

---
+++ Assets/Resources/Scripts/Mouse Controls/Selection/SizeBasedOnObject.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SizeBasedOnObject : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	public void SetSize(Bounds bounds)
+	{
+		GetComponent<Projector> ().orthographicSize = ((bounds.size.x + bounds.size.y + bounds.size.z) / 3.0f);
+	}
+}
