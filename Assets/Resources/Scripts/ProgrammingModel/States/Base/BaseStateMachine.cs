3d1ccd8473d0430636bc31a97a8ce3f07ed0df32
Demo Kioussis <Demo.kioussis@gmail.com>
2017-04-08 01:09:24 -0400

    Base state stuff set up

---
+++ Assets/Resources/Scripts/ProgrammingModel/States/Base/BaseStateMachine.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+[RequireComponent(typeof(Animator))]
+public class BaseStateMachine : MonoBehaviour {
+
+    protected Animator stateMachine;
+    public bool test;
+
+    void Awake() {
+        stateMachine = GetComponent<Animator>();
+        BaseStateBehaviour[] behaviours = stateMachine.GetBehaviours<BaseStateBehaviour>();
+        foreach (BaseStateBehaviour b in behaviours) {
+            b.setStateMachine(this);
+        }
+    }
+
+
+    private void updateParameter(string s, int value) {
+        stateMachine.SetInteger(s, value);
+    }
+    private void updateParameter(string s, float value)
+    {
+        stateMachine.SetFloat(s, value);
+    }
+    private void updateParameter(string s, bool value)
+    {
+        stateMachine.SetBool(s, value);
+    }
+
+
+}
---
+++ Assets/Resources/Scripts/ProgrammingModel/States/Unit/BaseUnit/BaseUnitStateMachine.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// use this to change parameters
+public class BaseUnitStateMachine : BaseStateMachine {
+
+    public bool defend, enemyInLOS, patrol, onTarget, reloading;
+    public RTSObject target;
+
+}
