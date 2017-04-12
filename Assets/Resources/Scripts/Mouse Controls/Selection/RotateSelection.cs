1d402fe32bf1e401716a31249e5f5022f107faad
Demo Kioussis <Demo.kioussis@gmail.com>
2017-04-12 16:33:04 -0400

    Fixed slection prefab

---
+++ Assets/Resources/Scripts/Mouse Controls/Selection/RotateSelection.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotateSelection : MonoBehaviour {
+
+    public float speedX, speedY, speedZ;
+
+
+	void Update () {
+        transform.Rotate(speedX * Time.deltaTime, speedY * Time.deltaTime, speedZ * Time.deltaTime);		
+	}
+}
