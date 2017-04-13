5c088444d9e81460c52bffde86cc4265790e5ef0
Dexter Kwok <dkwok.90@gmail.com>
2017-04-13 13:27:26 -0400

    Update menu controller

---
+++ Assets/MenuController.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuController : MonoBehaviour {
+
+	// Use this for initialization
+	public void Quit () {
+		Application.Quit ();
+	}
+	
+	// Update is called once per frame
+	public void GoToGame () {
+		Application.LoadLevel("Final");
+	}
+}
